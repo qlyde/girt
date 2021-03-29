afdb09e7f300bdd6212854063202e913714553f3
James Kroeger <jameskroeger123@gmail.com>
2021-03-30 03:00:32 +1100

    redirect error messages to stderr instead of stdout

--- girt-add.sh
+++ girt-add.sh
@@ -1,19 +1,19 @@
 #!/bin/dash
 
 if [ $# -eq 0 ]; then
-    echo "usage: $0 <filenames>"
+    echo "usage: $0 <filenames>" 1>&2
     exit 1
 elif [ ! -d .girt ]; then
-    echo "$0: error: girt repository directory .girt not found"
+    echo "$0: error: girt repository directory .girt not found" 1>&2
     exit 1
 fi
 
 for file in "$@"; do
     if [ ! -e "$file" ]; then
-        echo "$0: error: can not open '$file'"
+        echo "$0: error: can not open '$file'" 1>&2
         exit 1
     elif [ ! -f "$file" ]; then
-        echo "$0: error: '$file' is not a regular file"
+        echo "$0: error: '$file' is not a regular file" 1>&2
         exit 1
     fi
 done
