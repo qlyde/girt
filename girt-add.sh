da3f1b579f8f0d17a14c3aa1fdb17e011c1130d4
James Kroeger <jameskroeger123@gmail.com>
2021-03-30 01:38:14 +1100

    start girt-add error checking only

---
+++ girt-add.sh
@@ -0,0 +1,21 @@
+#!/bin/dash
+
+if [ $# -eq 0 ]; then
+    echo "usage: $0 <filenames>"
+    exit 1
+elif [ ! -d ".girt" ]; then
+    echo "$0: error: girt repository directory .girt not found"
+    exit 1
+fi
+
+for file in "$@"; do
+    if [ ! -e "$file" ]; then
+        echo "$0: error: can not open '$file'"
+        exit 1
+    elif [ ! -f "$file" ]; then
+        echo "$0: error: '$file' is not a regular file"
+        exit 1
+    fi
+done
+
+
