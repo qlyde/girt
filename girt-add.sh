0d8164a2ac6570076a30f41925051a2653d384f7
James Kroeger <jameskroeger123@gmail.com>
2021-03-30 21:43:26 +1100

    changed usage messages to use $0 instead of hardcoded filename

--- girt-add.sh
+++ girt-add.sh
@@ -1,19 +1,19 @@
 #!/bin/dash
 
 if [ $# -eq 0 ]; then
-    echo "usage: girt-add <filenames>" 1>&2
+    echo "usage: $0 <filenames>" 1>&2
     exit 1
 elif [ ! -d .girt ]; then
-    echo "girt-add: error: girt repository directory .girt not found" 1>&2
+    echo "$0: error: girt repository directory .girt not found" 1>&2
     exit 1
 fi
 
 for file in "$@"; do
     if [ ! -e "$file" ]; then
-        echo "girt-add: error: can not open '$file'" 1>&2
+        echo "$0: error: can not open '$file'" 1>&2
         exit 1
     elif [ ! -f "$file" ]; then
-        echo "girt-add: error: '$file' is not a regular file" 1>&2
+        echo "$0: error: '$file' is not a regular file" 1>&2
         exit 1
     fi
 done
--- girt-init.sh
+++ girt-init.sh
@@ -1,10 +1,10 @@
 #!/bin/dash
 
 if [ $# -ne 0 ]; then
-    echo "usage: girt-init" 1>&2
+    echo "usage: $0" 1>&2
     exit 1
 elif [ -e .girt ]; then
-    echo "girt-init: error: .girt already exists" 1>&2
+    echo "$0: error: .girt already exists" 1>&2
     exit 1
 fi
 
