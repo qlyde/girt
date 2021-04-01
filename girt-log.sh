afda9b19859ec931a8beed2bd1cb1ec600e16f82
James Kroeger <jameskroeger123@gmail.com>
2021-04-01 23:48:04 +1100

    finished girt-log

---
+++ girt-log.sh
@@ -0,0 +1,14 @@
+#!/bin/dash
+
+if [ $# -ne 0 ]; then
+    echo "usage: $0" 1>&2
+    exit 1
+elif [ ! -d .girt ]; then
+    echo "$0: error: girt repository directory .girt not found" 1>&2
+    exit 1
+fi
+
+for commit in .girt/objects/commits/*; do
+    message=$(cat -- "$commit" | grep '^message:' | sed s/^message://)
+    echo "$(basename -- "$commit")" "$message"
+done
