c7a6c5cf961b9e37e9a15df72bccb4819464837d
James Kroeger <jameskroeger123@gmail.com>
2021-04-02 00:42:07 +1100

    add functionality to girt-add removed files to remove them from the repo

--- girt-add.sh
+++ girt-add.sh
@@ -8,8 +8,24 @@ elif [ ! -d .girt ]; then
     exit 1
 fi
 
+head=$(cat .girt/HEAD)
+parent_commit=$(cat ".girt/$head")
+
 # check files exist and are regular before staging them
 for file in "$@"; do
+    # if file doesn't exist but exists in latest commit tree then continue
+    flag=
+    if [ ! -e "$file" -a -n "$parent_commit" ]; then
+        parent_tree=$(cat ".girt/objects/commits/$parent_commit" | grep '^tree:' | sed 's/^tree://')
+        while IFS= read -r line; do
+            parent_tree_file=$(echo "$line" | cut -d'/' -f1)
+            if [ "$file" = "$parent_tree_file" ]; then
+                flag=true
+            fi
+        done < ".girt/objects/trees/$parent_tree"
+        if [ -n "$flag" ]; then continue; fi
+    fi
+
     if [ ! -e "$file" ]; then
         echo "$0: error: can not open '$file'" 1>&2
         exit 1
@@ -20,15 +36,26 @@ for file in "$@"; do
 done
 
 for file in "$@"; do
-    mode=$(stat -c'%a' -- "$file")
-    hash=$(sha1sum -- "$file" | cut -d' ' -f1)
+    if [ ! -e "$file" -a -n "$parent_commit" ]; then
+        parent_tree=$(cat ".girt/objects/commits/$parent_commit" | grep '^tree:' | sed 's/^tree://')
+        while IFS= read -r line; do
+            parent_tree_file=$(echo "$line" | cut -d'/' -f1)
+            if [ "$file" = "$parent_tree_file" ]; then
+                # file doesn't exist but exists in latest commit tree so add to rmindex
+                echo "$file" >> .girt/rmindex
+            fi
+        done < ".girt/objects/trees/$parent_tree"
+    else
+        mode=$(stat -c'%a' -- "$file")
+        hash=$(sha1sum -- "$file" | cut -d' ' -f1)
 
-    # remove file from index if it exists
-    sed -i "/^$file\//d" .girt/index
+        # remove file from index if it exists
+        sed -i "/^$file\//d" .girt/index
 
-    # append to index
-    echo "$file/$mode/$hash" >> .girt/index
+        # append to index
+        echo "$file/$mode/$hash" >> .girt/index
 
-    # create blob
-    gzip -c -- "$file" > ".girt/objects/blobs/$hash"
+        # create blob
+        gzip -c -- "$file" > ".girt/objects/blobs/$hash"
+    fi
 done
--- girt-commit.sh
+++ girt-commit.sh
@@ -1,12 +1,27 @@
 #!/bin/dash
 
+# usage() { echo "usage: $0 [-a] -m commit-message" 1>&2; exit 1; }
+
+# while [ $# -gt 0 ]; do
+#     arg=$1
+#     case "$arg" in
+#         -a)
+#             shift
+#             ;;
+#         -m)
+#             shift 2
+#             ;;
+#         *) usage;;
+#     esac
+# done
+
 if [ ! -d .girt ]; then
     echo "$0: error: girt repository directory .girt not found" 1>&2
     exit 1
 fi
 
 # check if there is anything to commit
-if [ ! -s .girt/index ]; then
+if [ ! -s .girt/index -a ! -s .girt/rmindex ]; then
     echo "nothing to commit"
     exit 0
 fi
@@ -17,7 +32,7 @@ parent_commit=$(cat ".girt/$head")
 tree=$(cat .girt/index)
 if [ -n "$parent_commit" ]; then
     # add unchanged files to tree from parent commit, if parent exists
-    parent_tree=$(cat ".girt/objects/commits/$parent_commit" | grep '^tree:' | cut -d':' -f2)
+    parent_tree=$(cat ".girt/objects/commits/$parent_commit" | grep '^tree:' | sed 's/^tree://')
     while IFS= read -r line; do
         file=$(echo "$line" | cut -d'/' -f1)
         if ! echo "$tree" | grep -q "^$file/"; then
@@ -26,6 +41,9 @@ if [ -n "$parent_commit" ]; then
     done < ".girt/objects/trees/$parent_tree"
 fi
 
+while IFS= read -r file; do
+    tree=$(echo -n "$tree" | sed "/^$file\//d")
+done < ".girt/rmindex"
 tree_hash=$(echo "$tree" | sha1sum | cut -d' ' -f1)
 echo "$tree" > ".girt/objects/trees/$tree_hash"
 
@@ -42,3 +60,4 @@ echo "$commit_num" > ".girt/$head"
 
 # reset index
 echo -n > .girt/index
+echo -n > .girt/rmindex
