813d60fcf00c632cf3cbd73b8ff8dc56a1cca1eb
James Kroeger <jameskroeger123@gmail.com>
2021-04-02 20:02:11 +1100

    fixed error messages

--- girt-show.sh
+++ girt-show.sh
@@ -1,12 +1,12 @@
 #!/bin/dash
 
-usage() { echo "$0: error: girt repository directory .girt not found" 1>&2; exit 1; }
+usage() { echo "usage: $0 <commit>:<filename>" 1>&2; exit 1; }
 
 if [ ! -d .girt ]; then
-    usage
-elif [ $# -ne 1 ]; then
-    echo "usage: $0 <commit>:<filename>" 1>&2
+    echo "$0: error: girt repository directory .girt not found" 1>&2
     exit 1
+elif [ $# -ne 1 ]; then
+    usage
 fi
 
 # parse argument
