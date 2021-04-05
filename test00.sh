512a7d6389fe7db73ef4434f3073c831107720eb
James Kroeger <jameskroeger123@gmail.com>
2021-04-06 01:49:11 +1000

    finished test01, refined test00

--- test00.sh
+++ test00.sh
@@ -4,21 +4,26 @@
 failed() { echo "$@" && exit 1; }
 passed() { echo "all tests passed" && exit 0; }
 
-[ -d .girt ] && rm -rf .girt
+TEST_DIR=".test00"
 
-# test success
-out=$(girt-init)
+[ -e $TEST_DIR ] && rm -rf $TEST_DIR
+PATH=..:$PATH
+mkdir $TEST_DIR && cd $TEST_DIR
+
+# test 1: success
+out=$(girt-init 2>&1)
 out_exp="Initialized empty girt repository in .girt"
-test "$out" = "$out_exp" || failed "$0: failed: incorrect output: expected '$out_exp', got '$out'"
-test -d .girt || failed "$0: failed: .girt directory doesn't exist"
+test "$out" = "$out_exp" || failed "$0: test 1 failed: incorrect output: expected '$out_exp', got '$out'"
+test -d .girt || failed "$0: test 1 failed: .girt directory not found"
+echo "test 1 passed"
 
-# test failure
+# test 2: failure
 out=$(girt-init 2>&1)
 out_status=$?
 out_exp=".girt already exists"
-test $out_status -eq 1 || failed "$0: failed: exit status 1 expected, got $out_status"
-echo "$out" | grep -Fq "$out_exp" || failed "$0: failed: incorrect output: expected '$out_exp', got '$out'"
+test $out_status -eq 1 || failed "$0: test 2 failed: exit status 1 expected, got $out_status"
+echo "$out" | grep -Fq "$out_exp" || failed "$0: test 2 failed: incorrect output: expected '$out_exp', got '$out'"
+echo "test 2 passed"
 
-rm -rf .girt
-
+cd .. && rm -rf $TEST_DIR
 passed
--- test01.sh
+++ test01.sh
@@ -1,1 +1,59 @@
 #!/bin/dash
+# girt-add test
+
+failed() { echo "$@" && exit 1; }
+passed() { echo "all tests passed" && exit 0; }
+
+test_failure() {
+    testno=$1
+    out=$2
+    out_status=$3
+    out_exp=$4
+    test $out_status -eq 1 || failed "$0: test $testno failed: exit status 1 expected, got $out_status"
+    echo "$out" | grep -Fq "$out_exp" || failed "$0: test $testno failed: incorrect output: expected '$out_exp', got '$out'"
+}
+
+TEST_DIR=".test01"
+
+[ -e $TEST_DIR ] && rm -rf $TEST_DIR
+PATH=..:$PATH
+mkdir $TEST_DIR && cd $TEST_DIR
+
+# test 1: failure: no .girt directory
+out=$(girt-add 2>&1)
+out_status=$?
+out_exp="girt repository directory .girt not found"
+test_failure 1 "$out" "$out_status" "$out_exp"
+echo "test 1 passed"
+
+girt-init > /dev/null 2>&1
+
+# test 2: failure: non-existent file
+out=$(girt-add non_existent_file 2>&1)
+out_status=$?
+out_exp="can not open 'non_existent_file'"
+test_failure 2 "$out" "$out_status" "$out_exp"
+echo "test 2 passed"
+
+# test 3: failure: non-regular file
+mkdir some_directory
+out=$(girt-add some_directory 2>&1)
+out_status=$?
+out_exp="'some_directory' is not a regular file"
+test_failure 3 "$out" "$out_status" "$out_exp"
+echo "test 3 passed"
+
+# test 4: success
+touch a
+out=$(girt-add a 2>&1)
+test -z "$out" || failed "$0: test 4 failed: got unexpected output '$out'"
+echo "test 4 passed"
+
+# test 5: success: adding a removed file
+rm a
+out=$(girt-add a 2>&1)
+test -z "$out" || failed "$0: test 5 failed: got unexpected output '$out'"
+echo "test 5 passed"
+
+cd .. && rm -rf $TEST_DIR
+passed
